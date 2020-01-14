6382d8e433aeaf1c0a496abcf477f680cd8a55d0
Jacob Albright <jacob.albright23@gmail.com>
2020-01-14 09:53:03 -0600

    providing reactive breakpoint object from layouts/game.vue

---
+++ layouts/game.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <TheNavbar />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import TheNavbar from '@/components/the-navbar'
+export default {
+  components: {
+    TheNavbar,
+  },
+  data() {
+    return {
+      windowSize: {
+        width: 0,
+        height: 0,
+      },
+      rules: {
+        isMobile: w => w < 769,
+        isTablet: w => w >= 769 && w < 1024,
+        isDesktop: w => w >= 1024 && w < 1216,
+        isWidescreen: w => w >= 1216 && w < 1408,
+        isFullhd: w => w >= 1408,
+
+        aboveMobile: w => w >= 769,
+        aboveTablet: w => w >= 1024,
+        aboveDesktop: w => w >= 1216,
+        
+        belowDesktop: w => w < 1024,
+        belowWidescreen: w => w < 1216,
+        belowFullhd: w => w < 1408,
+
+      },
+      breakpoint: {
+        isMobile: false,
+        isTablet: null,
+        isDesktop: null,
+        isWidescreen: null,
+        isFullhd: null,
+        aboveMobile: null,
+        aboveTablet: null,
+        aboveDesktop: null,
+        belowDesktop: null,
+        belowWidescreen: null,
+        belowFullhd: null,
+      },
+    }
+  },
+  provide() {
+    return {
+      breakpoint: this.breakpoint,
+      windowSize: this.windowSize,
+    }
+  },
+  methods: {
+    onResize() {
+      this.windowSize.width = window.innerWidth
+      this.windowSize.height = window.innerHeight
+
+      Object.keys(this.rules).forEach(key => {
+        this.breakpoint[key] = this.rules[key](window.innerWidth)
+      })
+    }
+  },
+  created() {
+    if (process.client) {
+      this.onResize()
+      window.addEventListener('resize', this.onResize)
+    }
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.onResize)
+  }
+}
+</script>
--- pages/games.vue
+++ pages/games.vue
@@ -3,3 +3,13 @@
     <nuxt-child></nuxt-child>
   </div>
 </template>
+
+<script>
+export default {
+  layout(ctx) {
+    if (ctx.params.game) {
+      return 'game'
+    }
+  },
+}
+</script>
