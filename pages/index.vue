bf38979dd36cf3f4dbe5776344b8b64a4b032bd3
Jacob Albright <jacob.albright23@gmail.com>
2020-01-18 11:07:37 -0600

    wrote classes for responsive hiding and showing

--- pages/index.vue
+++ pages/index.vue
@@ -30,51 +30,49 @@
         </div>
       </article>
     </j-col>
-    <client-only>
-      <j-col v-if="breakpoint.aboveMobile" class="bg-dark--darker px-4 d-flex flex-column justify-center">
-        <nuxt-link class="card flat pa-0 my-4 text-light" to="/games">
-          <div class="card-title text-light text-underlined pb-0">
-            Games
-          </div>
-          <div class="card-text">
-            <p class="my-0">
-              Have fun with some classic arcade games I've implemented with my own spin using
-              HTML Canvas and Javascript.
-            </p>
-          </div>
-        </nuxt-link>
-        <nuxt-link class="card hov-orange flat pa-0 mb-4 text-light" to="/photos">
-          <div class="card-title text-light text-underlined pb-0">
-            Photos
-          </div>
-          <div class="card-text">
-            <p class="my-0">
-              There isn't anything here yet ...
-            </p>
-          </div>
-        </nuxt-link>
-        <nuxt-link class="card hov-purple flat pa-0 mb-4 text-light" to="/thoughts">
-          <div class="card-title text-light text-underlined pb-0">
-            Thoughts
-          </div>
-          <div class="card-text">
-            <p class="my-0">
-              This is where I write my thoughts about things.
-            </p>
-          </div>
-        </nuxt-link>
-        <nuxt-link class="card hov-green flat pa-0 mb-4 text-light" to="/projects">
-          <div class="card-title text-light text-underlined pb-0">
-            Projects
-          </div>
-          <div class="card-text">
-            <p class="my-0">
-              Take a look at some projects I've worked on.
-            </p>
-          </div>
-        </nuxt-link>
-      </j-col>
-    </client-only>
+    <j-col class="bg-dark--darker px-4 d-flex flex-column justify-center hidden-mobile">
+      <nuxt-link class="card flat pa-0 my-4 text-light" to="/games">
+        <div class="card-title text-light text-underlined pb-0">
+          Games
+        </div>
+        <div class="card-text">
+          <p class="my-0">
+            Have fun with some classic arcade games I've implemented with my own spin using
+            HTML Canvas and Javascript.
+          </p>
+        </div>
+      </nuxt-link>
+      <nuxt-link class="card hov-orange flat pa-0 mb-4 text-light" to="/photos">
+        <div class="card-title text-light text-underlined pb-0">
+          Photos
+        </div>
+        <div class="card-text">
+          <p class="my-0">
+            There isn't anything here yet ...
+          </p>
+        </div>
+      </nuxt-link>
+      <nuxt-link class="card hov-purple flat pa-0 mb-4 text-light" to="/thoughts">
+        <div class="card-title text-light text-underlined pb-0">
+          Thoughts
+        </div>
+        <div class="card-text">
+          <p class="my-0">
+            This is where I write my thoughts about things.
+          </p>
+        </div>
+      </nuxt-link>
+      <nuxt-link class="card hov-green flat pa-0 mb-4 text-light" to="/projects">
+        <div class="card-title text-light text-underlined pb-0">
+          Projects
+        </div>
+        <div class="card-text">
+          <p class="my-0">
+            Take a look at some projects I've worked on.
+          </p>
+        </div>
+      </nuxt-link>
+    </j-col>
   </j-row>
 </template>
 
